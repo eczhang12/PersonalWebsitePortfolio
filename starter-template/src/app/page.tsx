6d0cc06bafc7988e79b6dd436ebbf7db9a08a836
Evan <goodevan7@gmail.com>
2025-11-05 18:39:40 -0600

    Fixed header fixed scrolling. Need to update PI

--- starter-template/src/app/page.tsx
+++ starter-template/src/app/page.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/sections/Header";
 import { HeroSection } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
 import { TapeSection } from "@/sections/Tape";
-import { AboutSection} from "@/sections/About";
+import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
 import { Footer } from "@/sections/Footer";
 
@@ -10,12 +10,28 @@ export default function Home() {
   return (
     <div>
       <Header />
-      <HeroSection />
-      <ProjectsSection />
-      <TapeSection />
-      <AboutSection />
-      <ContactSection />
-        <Footer />
+
+      <section id="home">
+        <HeroSection />
+      </section>
+
+      <section id="projects" className="scroll-mt-24">
+        <ProjectsSection />
+      </section>
+
+      <section id="tape" className="scroll-mt-24">
+        <TapeSection />
+      </section>
+
+      <section id="about" className="scroll-mt-24">
+        <AboutSection />
+      </section>
+
+      <section id="contact" className="scroll-mt-24">
+        <ContactSection />
+      </section>
+
+      <Footer />
     </div>
   );
 }
